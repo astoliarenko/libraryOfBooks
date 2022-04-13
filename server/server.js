b9a9349f29cfc8698eb4374c49c225cd3b3f5b34
Stolyarenko <astoliarenko@xbsoftware.by>
2022-04-13 12:49:13 +0300

    [feature][server]: installed cookie-parser package, added options for cors

--- server/server.js
+++ server/server.js
@@ -5,7 +5,12 @@ const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 3500;
 
-app.use(cors());
+const corsOptions = {
+	origin: "http://localhost:8080",
+	credentials: true,
+};
+
+app.use(cors(corsOptions));
 // .urlencoded анализирует текст в виде URL-кодированных данных (как браузеры, как правило,
 // 	отправляют данные формы из обычных форм, установленных в POST) и предоставляет
 // 	результирующий объект (содержащий ключи и значения) для req.body.
