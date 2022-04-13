53d5c2ee278eae5644a468afdd58d9ffb6dba788
Stolyarenko <astoliarenko@xbsoftware.by>
2022-04-13 13:37:31 +0300

    [fix][client, session.js]: added credentials header for fetch

--- client/sources/models/session.js
+++ client/sources/models/session.js
@@ -9,12 +9,14 @@ function login(user, pass, isRemember) {
 	return	fetch(`${constants.URLs.SERVER}auth/login`, {
 			method: "POST",
 			// mode: 'no-cors', // no-cors, *cors, same-origin
+			credentials: "include", // omit, same-origin*
+			// mode: 'no-cors',
 			headers: {
 				"Content-Type": "application/json",
 			},
 			body: JSON.stringify({username: user, password: pass, isRemember})
 		})
-			.then((response) => response.json());
+			.then(res => res.json());
 }
 
 function logout() {
