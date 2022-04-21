f353d252bb804e1ba58ff026d13d4c6e7ed48e37
Stolyarenko <astoliarenko@xbsoftware.by>
2022-04-21 16:44:53 +0300

    [refactor][client]: profile view was changed

--- client/sources/views/reader/profile.js
+++ client/sources/views/reader/profile.js
@@ -5,8 +5,8 @@ import userInfoColumns from "../../data/userInfoColumns";
 
 export default class ProfileView extends JetView {
 	config() {
-		const labelWidth = 100;
-		const formWidth = 300;
+		const labelWidth = 120;
+		const formWidth = 800;
 
 		const applyChangesBtn = {
 			view: "button",
@@ -47,87 +47,105 @@ export default class ProfileView extends JetView {
 
 		const form = {
 			view: "form",
+			borderless: true,
 			localId: constants.IDs.USER_INFO_FORM,
 			width: formWidth,
+			elementsConfig: {
+				margin: 10
+			},
 			elements: [
 				{
 					rows: [
 						{
-							view: "text",
-							label: "Имя",
-							name: userInfoColumns.firstName,
-							labelWidth
-						},
-						{
-							view: "text",
-							label: "Фамилия",
-							name: "secondName",
-							labelWidth
-						},
-						{
-							view: "text",
-							label: "Отчество",
-							name: "thirdName",
-							labelWidth
-						},
-						{
-							view: "text",
-							label: "Номер паспорта",
-							name: userInfoColumns.passportNumber,
-							labelWidth
-						},
-						{
-							view: "datepicker",
-							value: "",
-							name: userInfoColumns.birthDate,
-							label: "Дата рождения",
-							timepicker: false,
-							format: webix.Date.dateToStr(constants.DATE_FORMAT),
-							labelWidth
-						},
-						{
-							view: "textarea",
-							label: "Адрес",
-							name: userInfoColumns.address,
-							labelWidth
-						},
-						{
-							view: "text",
-							label: "Телефон-1",
-							name: userInfoColumns.phoneNumber1,
-							labelWidth
-						},
-						{
-							view: "text",
-							label: "Телефон-2",
-							name: userInfoColumns.phoneNumber2,
-							labelWidth
-						},
-						{
-							view: "text",
-							label: "Телефон-3",
-							name: userInfoColumns.phoneNumber3,
-							labelWidth
-						},
-						{
-							view: "text",
-							label: "Телефон-4",
-							name: userInfoColumns.phoneNumber4,
-							labelWidth
-						},
-						{
-							view: "text",
-							label: "Логин",
-							name: "login",
-							labelWidth
-						},
-						{
-							view: "text",
-							label: "Password",
-							type: "password",
-							name: "password",
-							invalidMessage: "Ent. year between 1970 and cur.",
-							labelWidth
+							cols: [
+								{
+									gravity: 1,
+									rows: [
+										{
+											view: "text",
+											label: "Имя",
+											name: userInfoColumns.firstName,
+											labelWidth
+										},
+										{
+											view: "text",
+											label: "Фамилия",
+											name: "secondName",
+											labelWidth
+										},
+										{
+											view: "text",
+											label: "Отчество",
+											name: "thirdName",
+											labelWidth
+										},
+										{
+											view: "text",
+											label: "Номер паспорта",
+											name: userInfoColumns.passportNumber,
+											labelWidth
+										},
+										{
+											view: "datepicker",
+											value: "",
+											name: userInfoColumns.birthDate,
+											label: "Дата рождения",
+											timepicker: false,
+											format: webix.Date.dateToStr(constants.DATE_FORMAT),
+											labelWidth
+										},
+										{
+											view: "textarea",
+											label: "Адрес",
+											name: userInfoColumns.address,
+											labelWidth
+										}
+									]
+								},
+								{
+									gravity: 1,
+									rows: [
+										{
+											view: "text",
+											label: "Телефон-1",
+											name: userInfoColumns.phoneNumber1,
+											labelWidth
+										},
+										{
+											view: "text",
+											label: "Телефон-2",
+											name: userInfoColumns.phoneNumber2,
+											labelWidth
+										},
+										{
+											view: "text",
+											label: "Телефон-3",
+											name: userInfoColumns.phoneNumber3,
+											labelWidth
+										},
+										{
+											view: "text",
+											label: "Телефон-4",
+											name: userInfoColumns.phoneNumber4,
+											labelWidth
+										}
+									// {
+									// 	view: "text",
+									// 	label: "Логин",
+									// 	name: "login",
+									// 	labelWidth
+									// },
+									// {
+									// 	view: "text",
+									// 	label: "Password",
+									// 	type: "password",
+									// 	name: "password",
+									// 	invalidMessage: "Ent. year between 1970 and cur.",
+									// 	labelWidth
+									// }
+									]
+								}
+							]
 						},
 						{
 							cols: [
@@ -144,7 +162,14 @@ export default class ProfileView extends JetView {
 			}
 		};
 
-		return form;
+		const ui = {
+			type: "clean",
+			cols: [
+				form, {}
+			]
+		};
+
+		return ui;
 	}
 
 	$$form() {
