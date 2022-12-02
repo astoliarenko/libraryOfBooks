a24b7ce562555d2b61bbf226b1d336aaa2ea351c
Aleksey Stolyarenko <astoliarenko@xbsoftware.by>
2022-12-02 16:36:04 +0300

    feat: added const

---
+++ client/sources/helpers/constants/commonConst.ts
@@ -0,0 +1,14 @@
+/**
+* @module CommonConstants
+*/
+/**
+* @constant {object} eventsName - list of global events
+*/
+export const eventsName = {
+    refreshTokenError: 'app:refreshTokenError',
+};
+
+/**
+* @constant {object} globalIds - list of global ids
+*/
+export const globalIds = {};
