9f2ed56d2d8798cb3101531f5b5d604287fe315f
Aleksey Stolyarenko <astoliarenko@xbsoftware.by>
2023-01-23 16:50:51 +0300

    feat: added auth repository

--- server/modules/authorization/authorization.repository.js
+++ server/modules/authorization/authorization.repository.js
@@ -1,5 +1,32 @@
-class AuthorizationRepository {
+const { DB } = require("../../constants");
+const db = require("../../settings/db");
+const util = require("util");
+const promisifyDbQuery = util.promisify(db.query.bind(db));
 
+class AuthorizationRepository {
+	async getUserByLogin(login) {
+		return promisifyDbQuery(`
+			SELECT *
+			FROM \`${DB.USERS.NAME}\`
+			WHERE \`${DB.USERS.COLUMNS.LOGIN}\` = '${login}'
+		`);
+	}
+
+	async getUserById(userId) {
+		return promisifyDbQuery(`
+			SELECT *
+			FROM \`${DB.USERS_INFO.NAME}\`
+			WHERE \`${DB.USERS.COLUMNS.USER_ID}\` = '${userId}'
+		`);
+	}
+
+	async addNewUser(userData) {
+		return promisifyDbQuery(`
+			INSERT INTO \`${DB.USERS.NAME}\`
+			(\`${DB.USERS.COLUMNS.LOGIN}\`, \`${DB.USERS.COLUMNS.PASSWORD}\`, \`${DB.USERS.COLUMNS.ROLE_ID}\`)
+			VALUES('${userData.username}', '${userData.password}', '${userData.role}')
+		`);
+	}
 }
 
 module.exports = new AuthorizationRepository();
