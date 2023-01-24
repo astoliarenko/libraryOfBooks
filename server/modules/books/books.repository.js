b20a89ed54bce8ad7897bf50e8b3993202e4145a
Aleksey Stolyarenko <astoliarenko@xbsoftware.by>
2023-01-24 18:04:10 +0300

    fix: changed books.rep

--- server/modules/books/books.repository.js
+++ server/modules/books/books.repository.js
@@ -4,11 +4,17 @@ const promisifyDbQuery = util.promisify(db.query.bind(db));
 
 class BooksRepository {
 	async getAllBooks() {
+		// TODO: get all info
+		// return promisifyDbQuery(`
+		// 	SELECT *
+		// 	FROM \`books\`
+		// 	LEFT JOIN \`authors\`
+		// 	ON \`books.id_author\` = \`authors.id_author\`
+		// `);
+
 		return promisifyDbQuery(`
 			SELECT *
 			FROM \`books\`
-			LEFT JOIN \`authors\`
-			ON \`books.id_author\` = \`authors.id_author\`
 		`);
 	}
 }
