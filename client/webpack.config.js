71ffa23d46181c57f48d949ea6f9e152fbf59273
Aleksey Stolyarenko <astoliarenko@xbsoftware.by>
2023-01-30 16:23:09 +0300

    feat: added sass loader

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -35,6 +35,17 @@ module.exports = function (env) {
 				{
 					test: /\.(less|css)$/,
 					use: [MiniCssExtractPlugin.loader, "css-loader", "less-loader"]
+				},
+				{
+					test: /\.s[ac]ss$/i,
+					use: [
+						// Creates `style` nodes from JS strings
+						"style-loader",
+						// Translates CSS into CommonJS
+						"css-loader",
+						// Compiles Sass to CSS
+						"sass-loader"
+					]
 				}
 			]
 		},
