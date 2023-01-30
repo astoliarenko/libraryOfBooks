6fd407ab6074e44a675303a6e06d7bb2e67dfc60
Aleksey Stolyarenko <astoliarenko@xbsoftware.by>
2023-01-30 14:05:56 +0300

    feat: added modifyBooksData method

--- client/sources/models/books.ts
+++ client/sources/models/books.ts
@@ -3,8 +3,32 @@ import { getRequestOptions } from "./fetchApiService";
 import IQueryResult from "sources/interfaces/IQueryResult";
 import { showMessage } from "./utils";
 
+interface Genre {
+    id_genre: number,
+    name: string
+}
+
+interface Book {
+    id_book: number,
+    author: string,
+    book_title: string,
+    genres: string,
+    year_of_publishing: number,
+    publishing_house: string,
+    number_of_pages: number,
+    number_of_available_copies: number,
+    first_name: string,
+    last_name: string,
+    cover_photo: string,
+    country_of_publication: string,
+    book_file: string,
+    audio_recording: string       
+}
+
 export default class BooksModel extends BaseModel {
 	protected static instance: BooksModel;
+    private genres: Genre[];
+    private booksData: Book[];
 
     private constructor() {
         super();
@@ -26,7 +50,8 @@ export default class BooksModel extends BaseModel {
             {
                 200: (response, result: IQueryResult) => {
                     result.success = true;
-                    result.data = response.data;
+                    this.booksData = this.modifyBooksData(response.data);
+                    result.data = this.booksData;
                     return result;
                 },
                 401: (response, result: IQueryResult) => {
@@ -42,6 +67,18 @@ export default class BooksModel extends BaseModel {
             },
             'get'
         );
+    }
+
+    public setGenresData(data: Genre[]) {
+        this.genres = data;
+    }
+
+    public getGenresData(): Genre[] {
+        return this.genres;
+    }
+
+    public getBooksData(): Book[] {
+        return this.booksData;
     }
 
     async getBookGenres() {
@@ -67,5 +104,18 @@ export default class BooksModel extends BaseModel {
             },
             'get'
         );
+    }
+
+    private modifyBooksData(books: Book[]): Book[] {
+        if (this.genres) {
+            return books.map((book) => {
+                book.genres = book.genres.split(',').map((genreId) => {
+                    return this.genres.find(genre => genre.id_genre === Number(genreId)).name;
+                }).join(', ');
+
+                return book;
+            });
+        }
+        else return books;
     }
 }
