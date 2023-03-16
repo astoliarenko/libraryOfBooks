ee95f4d60b55fcf6534db0410f1e96decc973f55
Aleksey Stolyarenko <astoliarenko@xbsoftware.by>
2023-03-16 15:16:34 +0300

    fix: changed generatePhonenumberTextInputConfig

--- client/sources/helpers/inputs.ts
+++ client/sources/helpers/inputs.ts
@@ -1,11 +1,21 @@
-const generatePhonenumberTextInputConfig = (label: string, name: string, labelWidth?: number) => {
-	return {
+const generatePhonenumberTextInputConfig = (label: string, name: string, labelWidth: number, additional?: webix.ui.textConfig) => {
+	let config = {
 		view: "text",
 		label,
 		name,
-		labelWidth: labelWidth ? labelWidth : 200,
-		pattern: {mask:"+### (##) ###-##-##", allow:/[0-9]/g}
+		labelWidth: labelWidth,
+		pattern: {mask:"+### (##) ###-##-##", allow:/[0-9]/g},
+		...additional
 	} as webix.ui.textConfig;
+
+	if (additional) {
+		config = {
+			...config,
+			...additional
+		}
+	}
+
+	return config;
 };
 
 export default generatePhonenumberTextInputConfig;
