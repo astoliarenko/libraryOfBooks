0bbe12546f4b90ab268b33f853bcb6aadd407d19
Aleksey Stolyarenko <astoliarenko@xbsoftware.by>
2023-03-16 15:17:02 +0300

    refactor: added generateNameTextInputConfig

--- client/sources/views/authorization/registerWindow.ts
+++ client/sources/views/authorization/registerWindow.ts
@@ -23,7 +23,7 @@ const formNames = {
 export default class RegisterWindowView extends JetView {
 	config() {
 		const btnWidth = 150;
-		const labelWidth = 120;
+		const labelWidth = 160;
 		const formWidth = 400;
 
 		const btnRegisterNewUser = {
@@ -37,40 +37,43 @@ export default class RegisterWindowView extends JetView {
 		const btnCancel = {
 			view: "button",
 			width: btnWidth,
-			value: "Cancel",
-			click: () => this.hideWindow()
+			value: "Clear",
+			click: () => this.clearForm()
 		};
 
 		const form = {
 			view: "form",
 			localId: constants.AUTHORIZATION_VIEW.REGISTER.FORM_ID,
 			width: formWidth,
+			padding: {top: 18, left: 17, right: 18, bottom: 7},
 			elements: [
 				{
 					rows: [
-						{
-							view: "text",
-							label: "Имя",
-							name: formNames.firstName,
+						this.generateNameTextInputConfig(
+							"Имя",
+							formNames.firstName,
+							true,
 							labelWidth
-						},
-						{
-							view: "text",
-							label: "Фамилия",
-							name: formNames.secondName,
+						),
+						this.generateNameTextInputConfig(
+							"Фамилия",
+							formNames.secondName,
+							true,
 							labelWidth
-						},
-						{
-							view: "text",
-							label: "Отчество",
-							name: formNames.thirdName,
+						),
+						this.generateNameTextInputConfig(
+							"Отчество",
+							formNames.thirdName,
+							false,
 							labelWidth
-						},
+						),
 						{
 							view: "text",
 							label: "Номер паспорта",
 							name: formNames.passportNumber,
-							labelWidth
+							labelWidth,
+							pattern: {mask:"##-#######", allow:/[0-9]/g},
+							required: true
 						},
 						{
 							view: "datepicker",
@@ -85,24 +88,45 @@ export default class RegisterWindowView extends JetView {
 							view: "textarea",
 							label: "Адрес",
 							name: formNames.address,
-							labelWidth
+							labelWidth,
+							attributes: {maxlength: 120}
 						},
-						generatePhonenumberTextInputConfig("Телефон-1", formNames.phone1, labelWidth),
-						generatePhonenumberTextInputConfig("Телефон-2", formNames.phone2, labelWidth),
-						generatePhonenumberTextInputConfig("Телефон-3", formNames.phone3, labelWidth),
-						generatePhonenumberTextInputConfig("Телефон-4", formNames.phone4, labelWidth),
+						generatePhonenumberTextInputConfig("Телефон-1", formNames.phone1, labelWidth, {on: {
+							onChange: (newValue, oldValue) => {
+								if (newValue.length === 12) {
+									this.$$form.queryView({name: formNames.phone2}).show();
+								}
+							}
+						}}),
+						generatePhonenumberTextInputConfig("Телефон-2", formNames.phone2, labelWidth, {hidden: true, on: {
+							onChange: (newValue, oldValue) => {
+								if (newValue.length === 12) {
+									this.$$form.queryView({name: formNames.phone3}).show();
+								}
+							}
+						}}),
+						generatePhonenumberTextInputConfig("Телефон-3", formNames.phone3, labelWidth, {hidden: true, on: {
+							onChange: (newValue, oldValue) => {
+								if (newValue.length === 12) {
+									this.$$form.queryView({name: formNames.phone4}).show();
+								}
+							}
+						}}),
+						generatePhonenumberTextInputConfig("Телефон-4", formNames.phone4, labelWidth, {hidden: true}),
 						{
 							view: "text",
 							label: "Номер карточки",
 							name: formNames.cardId,
 							pattern: {mask:"##-##-##", allow:/[0-9]/g},
-							labelWidth
+							labelWidth,
+							required: true
 						},
 						{
 							view: "text",
 							label: "Логин",
 							name: formNames.login,
-							labelWidth
+							labelWidth,
+							required: true
 						},
 						{
 							view: "text",
@@ -110,21 +134,15 @@ export default class RegisterWindowView extends JetView {
 							type: "password",
 							name: formNames.password,
 							invalidMessage: "Ent. year between 1970 and cur.",
-							labelWidth
-						},
-						{
-							cols: [
-								btnRegisterNewUser,
-								{},
-								btnCancel
-							]
+							labelWidth,
+							required: true
 						}
 					]
 				}
 			],
 			rules: {
-				secondName: webix.rules.isNotEmpty,
-				number_111: webix.rules.isNotEmpty
+				[formNames.firstName]: webix.rules.isNotEmpty,
+				[formNames.secondName]: webix.rules.isNotEmpty
 			}
 		} as webix.ui.formConfig;
 
@@ -132,15 +150,46 @@ export default class RegisterWindowView extends JetView {
 			localId: constants.AUTHORIZATION_VIEW.REGISTER.REGISTER_WINDOW_ID,
 			view: "window",
 			modal: true,
-			head: {localId: constants.AUTHORIZATION_VIEW.REGISTER.HEADER_ID, template: "Зарегистрировать"},
+			head: {
+				type: 'clean',
+				cols: [
+					{
+						localId: constants.AUTHORIZATION_VIEW.REGISTER.HEADER_ID,
+						template: "Зарегистрировать",
+						width: 200
+					},
+					{},
+					{
+						view: "icon",
+						icon: "wxi-close",
+						click: () => {
+							this.hideWindow();
+						}
+					}
+				]
+			},
 			position: "center",
-			body: {...wrapInScrollView('y', form), minHeight: 400}
+			body: {
+				rows: [
+					{
+						...wrapInScrollView('y', form), minHeight: 400
+					},
+					{
+						padding: {top: 18, left: 17, right: 18, bottom: 17},
+						cols: [
+							btnRegisterNewUser,
+							{},
+							btnCancel
+					]
+					}
+				]
+			}
 		};
 
 		return ui;
 	}
 
-	$$form(): webix.ui.form {
+	get $$form(): webix.ui.form {
 		return this.$$(constants.AUTHORIZATION_VIEW.REGISTER.FORM_ID) as unknown as webix.ui.form;
 	}
 
@@ -149,20 +198,42 @@ export default class RegisterWindowView extends JetView {
 		// eslint-disable-next-line no-console
 		console.log("register new user");
 
+		const form = this.$$form;
+		const values = form.getValues();
+
 		this.hideWindow();
 	}
 
 	hideWindow() {
-		const form = this.$$form();
+		this.clearForm();
+		// @ts-ignore
+		this.getRoot().hide();
+	}
+
+	private clearForm() {
+		const form = this.$$form;
 
 		form.clear();
 		form.clearValidation();
-		// @ts-ignore
-		this.getRoot().hide();
+
+		form.queryView({name: formNames.phone2}).hide();
+		form.queryView({name: formNames.phone3}).hide();
+		form.queryView({name: formNames.phone4}).hide();
 	}
 
 	showWindow() {
 		// @ts-ignore
 		this.getRoot().show();
+	}
+
+	private generateNameTextInputConfig(label: string, name: string, isRequired: boolean, labelWidth: number) {
+		return {
+			view: "text",
+			label,
+			name,
+			labelWidth,
+			required: isRequired,
+			attributes: {maxlength: 30}
+		} as webix.ui.textConfig
 	}
 }
