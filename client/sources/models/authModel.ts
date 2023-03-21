fe1108ec4b27a3e0867b503280585afa2c606c3a
Aleksey Stolyarenko <astoliarenko@xbsoftware.by>
2023-03-21 12:00:49 +0300

    refactor: auth model

--- client/sources/models/authModel.ts
+++ client/sources/models/authModel.ts
@@ -1,15 +1,14 @@
-import {events} from '../helpers/constants/commonConst';
-import {getItem, setItem} from '../helpers/storages/localAndSessionStorage';
+// import {events} from '../helpers/constants/commonConst';
+// import {getItem, setItem} from '../helpers/storages/localAndSessionStorage';
 
 import IQueryResult from '../interfaces/IQueryResult';
 import BaseModel from './baseModel';
-import {getRequestOptions, postRequestOptions} from './fetchApiService';
-import {showMessage} from './utils';
+import {postRequestOptions} from './fetchApiService';
+// import {showMessage} from './utils';
+import constants from '../constants';
 
 export default class AuthModel extends BaseModel {
     protected static instance: AuthModel;
-
-    private data: any;
 
     private constructor() {
         super();
@@ -24,106 +23,97 @@ export default class AuthModel extends BaseModel {
         return AuthModel.instance;
     }
 
-    public getUserInfo() {
-        if (!this.data) {
-            this.data = JSON.parse(getItem(localStorage, 'userData'));
-        }
-        return this.data;
+    private status() {
+        return webix.ajax().get(`${constants.URLs.SERVER}auth/status`)
+		    .then(res => res.json());
     }
 
-    public isReader() {
-        const userInfo = this.getUserInfo();
-        return !userInfo?.isAdmin && !userInfo?.isSuperAdmin;
-    }
-
-    public isAdmin() {
-        const userInfo = this.getUserInfo();
-        return userInfo?.isAdmin;
-    }
-
-    public isLibrarian() {
-        const userInfo = this.getUserInfo();
-        return userInfo?.isSuperAdmin;
-    }
-
-    async loginUser(body: {username: string, password: string, isRemember: boolean}) {
+    private loginUser(body: {username: string, password: string, isRemember: boolean}) {
         return this.handleRequestResponse(
-            postRequestOptions({path: '/auth/login', body}),
+            postRequestOptions({path: 'auth/login', body}),
             body,
             {
                 200: (response, result: IQueryResult) => {
                     result.success = true;
-                    result.data = response.data;
-                    this.data = response.data;
-                    setItem(localStorage, 'userData', JSON.stringify(response.data));
+                    result.data = {
+                        userInfo: response.userInfo,
+                        message: response.message
+                    };
                     return result;
                 },
-                401: (response, result: IQueryResult) => {
+                404: (response, result: IQueryResult) => {
                     result.success = false;
-                    result.data.message = response.data.message;
+                    result.data = {
+                        message: response.data.message,
+                        field: response.data.field
+                    }
                     return result;
                 },
-                422: () => {
-                    showMessage('Invalid user credentials', 'error');
+                422: (response, result: IQueryResult) => {
+                    result.success = false;
+                    result.data = {
+                        message: response.data.message,
+                        field: response.data.field
+                    }
+                    return result;
+                },
+                500: (response, result: IQueryResult) => {
+                    result.success = false;
+                    result.data = {
+                        message: response.data.message
+                    }
+                    return result;
                 }
-                // todo allow replace default
-                // showMessage('Something went wrong. Please try to login again', 'error');
             },
             'login'
         );
     }
 
-    async checkUser() {
-        return this.handleRequestResponse(
-            getRequestOptions({path: '/auth/user'}),
-            [],
-            {
-                200: (response, result: IQueryResult) => {
-                    result.success = true;
-                    this.data = response.data;
-                    setItem(localStorage, 'userData', JSON.stringify(response.data));
-                    return result;
-                }
-            },
-            'check user'
-        );
+    async cookieLogin() {
+        return	fetch(`${constants.URLs.SERVER}auth/login`, {
+            method: "GET",
+            // mode: 'no-cors', // no-cors, *cors, same-origin
+            credentials: "include", // omit, same-origin*
+            // mode: 'no-cors',
+            headers: {
+                "Content-Type": "application/json"
+            }
+        }).then(res => res.json());
     }
 
-    async refreshToken(app, repeatRequest) {
-        return this.handleRequestResponse(
-            getRequestOptions({path: '/auth/refresh'}),
-            [],
-            {
-                200: () => {
-                    return repeatRequest();
-                },
-                304: () => {
-                    return repeatRequest();
-                },
-                401: () => {
-                    // reset user data
-                    this.data = null;
-                    app.callEvent(events.app.refreshTokenError);
-                }
-            },
-            'refresh token'
-        );
-    }
+    // async refreshToken(app, repeatRequest) {
+    //     return this.handleRequestResponse(
+    //         getRequestOptions({path: '/auth/refresh'}),
+    //         [],
+    //         {
+    //             200: () => {
+    //                 return repeatRequest();
+    //             },
+    //             304: () => {
+    //                 return repeatRequest();
+    //             },
+    //             401: () => {
+    //                 // reset user data
+    //                 this.data = null;
+    //                 app.callEvent(events.app.refreshTokenError);
+    //             }
+    //         },
+    //         'refresh token'
+    //     );
+    // }
 
-    async logOutUser(body): Promise<IQueryResult> {
-        return this.handleRequestResponse(
-            postRequestOptions({path: '/auth/logout', body}),
-            body,
-            {200: (response: IQueryResult) => {
-                response.success = true;
-                // reset user data
-                this.data = null;
-                this.app.callEvent(events.app.refreshTokenError, [true]);
-                return response;
-            }},
-            'logout'
-        );
-    }
+    // async logOutUser(body): Promise<IQueryResult> {
+    //     return this.handleRequestResponse(
+    //         postRequestOptions({path: '/auth/logout', body}),
+    //         body,
+    //         {200: (response: IQueryResult) => {
+    //             response.success = true;
+    //             this.app.callEvent(events.app.refreshTokenError, [true]);
+    //             return response;
+    //         }},
+    //         'logout'
+    //     );
+    // }
 
     async registrationNewUser(body): Promise<IQueryResult> {
         return this.handleRequestResponse(
@@ -142,7 +132,7 @@ export default class AuthModel extends BaseModel {
                 return result;
             }
         },
-            'logout'
+            'registration new user'
         );
     }
 }
