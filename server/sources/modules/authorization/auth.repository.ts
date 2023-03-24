cddee02990e0555d60595d0f852fa68090276b23
Aleksey Stolyarenko <astoliarenko@xbsoftware.by>
2023-03-24 17:12:48 +0300

    refactor: used interfaces

--- server/sources/modules/authorization/auth.repository.ts
+++ server/sources/modules/authorization/auth.repository.ts
@@ -1,26 +1,9 @@
 import constants from "../../constants";
 import promisePool from "../../settings/db";
+
+import { userInfo, userInfoWithCreds } from "../interfaces/user";
+
 const DB = constants.DB;
-interface userInfo {
-	id_user: number,
-	first_name: string,
-	third_name: string | null,
-	last_name: string,
-	passport_number: number,
-	birthday: string,
-	adress: string,
-	card_id: number | null,
-	phone_1: string | null,
-	phone_2: string | null,
-	phone_3: string | null,
-	phone_4: string | null,
-}
-
-interface userInfoWithCreds extends userInfo {
-	login: string,
-	password: string,
-}
-
 interface newUserInfo {
 	first_name: string,
 	third_name?: string | null,
